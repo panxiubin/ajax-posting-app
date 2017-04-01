d824f6420dfc56d8bd0e2b06159ce8cd70b0503a
panxiubin <xiubinpan@gmail.com>
2017-04-01 13:31:12 +0800

    use thumbsup to like

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
       Like.create( :user => current_user, :post => @post )
     end
 
-    redirect_to posts_path
+    # redirect_to posts_path
   end
 
   def unlike
@@ -35,7 +35,8 @@ class PostsController < ApplicationController
     like = @post.find_like(current_user)
     like.destroy
 
-    redirect_to posts_path
+    # redirect_to posts_path
+    render "like"
   end
 
   protected
