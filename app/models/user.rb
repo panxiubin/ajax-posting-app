2560431cc2bdb9b78e94166ee6efe8c80d47af7d
panxiubin <xiubinpan@gmail.com>
2017-04-09 23:17:18 +0800

    bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
     self.email.split("@").first
   end
 
+  def is_admin?
+    role == "admin"
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
