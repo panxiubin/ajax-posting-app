f191555b0a3ec82ef691f17761a7fe091996576d
panxiubin <xiubinpan@gmail.com>
2017-04-10 19:45:22 +0800

    finish chapter4

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,15 @@ class Post < ApplicationRecord
 
   has_many :likes, :dependent => :destroy
   has_many :liked_users, :through => :likes, :source => :user
+  has_many :scores, :class_name => "PostScore"
+
+  def find_score(user)
+    user && self.scores.where( :user_id => user.id ).first
+  end
+
+  def average_score
+    self.scores.average(:score)
+  end
 
   def find_like(user)
     self.likes.where( :user_id => user.id ).first
